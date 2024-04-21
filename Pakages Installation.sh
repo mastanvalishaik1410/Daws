7ca968196352ea3af63aff16460988a0dc42767a
mastan <mastan@gmail>
2024-04-21 08:16:26 +0530

    Colors

--- Pakages Installation.sh
+++ Pakages Installation.sh
@@ -2,6 +2,21 @@
 
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0|cut -d "." -f1)
+LOGFILE=/tmp/$TIMESTAMP-$SCRIPT_NAME.log
+
+VALIDATE(){
+
+if [ $1 -ne 0 ]
+then
+	echo"$2 is fail"
+	exit 1
+else
+	echo "$2 is success"
+fi
+}
+
 
 if [ $USERID -ne 0 ]
 then
@@ -14,4 +29,9 @@ fi
 for i in $@
 do 
 	echo " packages to install $i"
+	dnf list installed $i &>>$LOGFILE
+if [ @? -eq 0]
+then
+	echo "$i already installed ...Skipping"
+fi
 done
