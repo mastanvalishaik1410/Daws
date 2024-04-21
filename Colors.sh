6629cedc4e91a16d2969314758646efb2f4658a2
mastan <mastan@gmail>
2024-04-21 07:08:01 +0530

    Colors

--- Colors.sh
+++ Colors.sh
@@ -15,10 +15,10 @@ VALIDATE(){
 
 if [ $1 -ne 0 ]
 then
-	echo -e"$2 is $R fail $N"
+	echo -e "$2 is $R fail $N"
 	exit 1
 else
-	echo "$2 is $G success $N"
+	echo -e "$2 is $G success $N"
 fi
 }
 if [ $USERID -ne 0 ]
