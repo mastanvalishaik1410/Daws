73c1ac80aa265dc559a90718f275dabd6d03b538
mastan <mastan@gmail>
2024-04-21 09:16:44 +0530

    Colors

--- Pakages Installation.sh
+++ Pakages Installation.sh
@@ -5,6 +5,10 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0|cut -d "." -f1)
 LOGFILE=/tmp/$TIMESTAMP-$SCRIPT_NAME.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[30m"
 
 VALIDATE(){
 
@@ -32,7 +36,7 @@ do
 	dnf list installed $i &>>$LOGFILE
 if [ $? -eq 0 ]
 then
-	echo "$i already installed ...Skipping"
+	echo "$i already installed ... $Y Skipping ..$N"
 else
 	echo "$i not installed...need to install"
 fi
